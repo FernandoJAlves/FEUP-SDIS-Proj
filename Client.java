a509e05a5766b304987db0ef5a9659438c3b480d
FernandoJAlves <up201605270@fe.up.pt>
2019-04-03 09:50:05 +0100

    Client code mostly done

--- Client.java
+++ Client.java
@@ -2,30 +2,101 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Client {
-    public Client() {}
+    public Client() {
+    }
+
+    public enum ProtocolList {
+        BACKUP, RESTORE, DELETE, RECLAIM, STATE
+    }
+
+    public static boolean containsProtocol(String test) {
+
+        for (ProtocolList m : ProtocolList.values()) {
+            if (m.name().equals(test)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String args[]) {
-       
-       if (args.length < 4 || args.length > 5) {
-            System.out.println("Usage: java Client <host_name> <remote_object_name> <oper> <opnd> *");
+
+        if (args.length > 4) {
+            System.out.println(
+                    "ERROR: Usage should be 'java Client <remote_object_name> <sub_protocol> <oper_1> [<oper_2>]'");
             return;
-       }
+        }
 
         try {
-            Registry registry = LocateRegistry.getRegistry(args[0]);
-            RemoteInterface stub = (RemoteInterface) registry.lookup(args[1]);
-            
-            switch (args.length) {
-                case 4:
-                    stub.lookup(args[3]);
+            String remotePeer = args[0];
+            String protocol = args[1];
+
+            if (containsProtocol(protocol)) {
+
+                // Only create stub if protocol is valid
+                Registry registry = LocateRegistry.getRegistry("localhost");
+                RemoteInterface stub = (RemoteInterface) registry.lookup(remotePeer);
+
+                switch (protocol) {
+                case "BACKUP": {
+                    if (args.length != 4) {
+                        System.out.println("ERROR: Incorrect number of arguments in BACKUP");
+                        return;
+                    }
+                    String filePath = args[2];
+                    int repDeg = Integer.parseInt(args[3]); // replication degree
+                    stub.backup(filePath, repDeg);
+                    System.out.println("BACKUP!");
                     break;
-                case 5:
-                    stub.register(args[3],args[4]);
+                }
+                case "RESTORE": {
+                    if (args.length != 3) {
+                        System.out.println("ERROR: Incorrect number of arguments in RESTORE");
+                        return;
+                    }
+                    String filePath = args[2];
+                    stub.restore(filePath);
+                    System.out.println("RESTORE!");
                     break;
+                }
+                case "DELETE": {
+                    if (args.length != 3) {
+                        System.out.println("ERROR: Incorrect number of arguments in DELETE");
+                        return;
+                    }
+                    String pathname = args[2];
+                    stub.delete(pathname);
+                    System.out.println("DELETE!");
+                    break;
+                }
+                case "RECLAIM": {
+                    if (args.length != 3) {
+                        System.out.println("ERROR: Incorrect number of arguments in RECLAIM");
+                        return;
+                    }
+                    int maxDiskSpace = Integer.parseInt(args[2]);
+                    stub.reclaim(maxDiskSpace);
+                    System.out.println("RECLAIM!");
+                    break;
+                }
+                case "STATE": {
+                    if (args.length != 2) {
+                        System.out.println("ERROR: Incorrect number of arguments in STATE");
+                        return;
+                    }
+                    stub.state();
+                    System.out.println("STATE!");
+                    break;
+                }
+
                 default:
+                    System.out.println("ERROR: Protocol Switch Entered Default");
                     break;
-            }
-        } catch(Exception e) {
+                }
+            } else
+                System.out.println("ERROR: Invalid Protocol");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
