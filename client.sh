c8e1cd18cd5a406073cf4638de37de75b89f0ffc
FernandoJAlves <up201605270@fe.up.pt>
2019-04-10 23:50:25 +0100

    Upgraded client.sh script

--- client.sh
+++ client.sh
@@ -1,20 +1,27 @@
 echo "Options: ";
-echo "0 - java Client localhost peer_3 BACKUP penguin.jpg 1;";
-echo "1 - java Client localhost peer_3 DELETE penguin.jpg;";
+echo "1 - java Client localhost peer_3 BACKUP penguin.jpg 1;";
+echo "2 - java Client localhost peer_3 DELETE penguin.jpg;";
+
+echo "0 - Close Options";
 echo Value: ;
 read val;
-sleep 1s;
 run_j_cmd () {
     case $1 in
-        0)
+        1)
             echo "java Client localhost peer_3 BACKUP penguin.jpg 1"
             java Client localhost peer_3 BACKUP penguin.jpg 1
         ;;
-        1)
+        2)
             echo "java Client localhost peer_3 DELETE penguin.jpg"
             java Client localhost peer_3 DELETE penguin.jpg
+        ;;
+        0)
+            echo "Leaving..."
+            exit 1
         ;;
     esac
 }
 
 run_j_cmd $val;
+sleep 1s;
+sh client.sh;
