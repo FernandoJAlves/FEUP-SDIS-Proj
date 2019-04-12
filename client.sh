dacdfddd35b04a9ad4038189876de4544ad010c4
Sandro Miguel Tavares Campos <up201605947@fe.up.pt>
2019-04-12 16:06:25 +0100

    restore protocol proto-implementation

--- client.sh
+++ client.sh
@@ -1,6 +1,10 @@
 echo "Options: ";
 echo "1 - java Client localhost peer_1 BACKUP qwerty.txt 1;";
-echo "2 - java Client localhost peer_1 DELETE penguin.jpg;";
+echo "2 - java Client localhost peer_1 DELETE qwerty.txt;";
+echo "3 - java Client localhost peer_1 RESTORE qwerty.txt;";
+echo "4 - java Client localhost peer_1 BACKUP penguin.jpg 1;";
+echo "5 - java Client localhost peer_1 DELETE penguin.jpg;";
+echo "6 - java Client localhost peer_1 RESTORE penguin.jpg;";
 
 echo "0 - Close Options";
 echo Value: ;
@@ -13,9 +17,25 @@ run_j_cmd () {
             # java Client 10.227.161.31 peer_1 BACKUP penguin.jpg 1
         ;;
         2)
+            echo "java Client localhost peer_1 DELETE qwerty.txt"
+            java Client localhost peer_1 DELETE qwerty.txt
+            # java Client 10.227.161.0 peer_1 DELETE penguin.jpg
+        ;;
+        3)
+            echo "java Client localhost peer_1 RESTORE qwerty.txt"
+            java Client localhost peer_1 RESTORE qwerty.txt
+        ;;
+        4)
+            echo "java Client localhost peer_1 BACKUP penguin.jpg 1"
+            java Client localhost peer_1 BACKUP penguin.jpg 1
+        ;;
+        5)
             echo "java Client localhost peer_1 DELETE penguin.jpg"
             java Client localhost peer_1 DELETE penguin.jpg
-            # java Client 10.227.161.0 peer_1 DELETE penguin.jpg
+        ;;
+        6)
+            echo "java Client localhost peer_1 RESTORE penguin.jpg"
+            java Client localhost peer_1 RESTORE penguin.jpg
         ;;
         0)
             echo "Leaving..."
